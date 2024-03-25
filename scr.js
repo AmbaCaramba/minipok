49507da50005823d8fbd846e77071879df45ce55
AmbaCaramba <4232478+AmbaCaramba@users.noreply.github.com>
2024-03-25 17:46:28 +0000

    deleting

--- scr.js
+++ scr.js
@@ -20,6 +20,13 @@ loadPokemonData();
 
 var card = document.querySelector('.card')
 
+card.addEventListener('transitionstart', function() {
+  data => {
+    pokemonImage.src = '';
+    pokemonName.textContent = '';
+    pokemonInfo.textContent = '';
+  }
+});
 
 card.addEventListener('transitionend', function() {
   if (!this.classList.contains('rotate')) {
