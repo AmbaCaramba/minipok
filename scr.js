24e42d24aa9376087b3ccd53cfefffb6d9842439
AmbaCaramba <4232478+AmbaCaramba@users.noreply.github.com>
2024-04-03 18:20:09 +0000

    font sizes

--- scr.js
+++ scr.js
@@ -10,7 +10,7 @@ function loadPokemonData() {
   fetch('https://pokeapi.co/api/v2/pokemon/' + (Math.floor(Math.random() * 898) + 1))
     .then(response => response.json())
     .then(data => {
-      pokemonImage.src = data.sprites.other['official-artwork'].front_default;
+      pokemonImage.src = data.sprites.other['official-artwork'].front_default;;
       pokemonName.textContent = data.name;
       pokemonInfo.textContent = data.abilities.map(ability => ability.ability.name).join(', ') + '.'
     });
